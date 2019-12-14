24cf2e68636ffcad71a42e74241ffc47ee573c36
tomoverman <tomovermanmhs@gmail.com>
2019-12-13 18:29:51 -0600

    add basic files

---
+++ main.py
@@ -0,0 +1,23 @@
+# integrate difficulty function numerically using composite trapezoidal rule
+def integrate_trap(dist, alt, n):
+    return value
+
+
+# integrate difficulty function numerically using composite simpson's rule
+def integrate_simp(dist, alt, n):
+    return value
+
+
+# find z values of natural cubic spline used to characterize the cubic spline interpolant
+def interp(x):
+    return value
+
+
+# find the value of the cubic spline interpolant at the given distance
+def cubic_spline(x):
+    return value
+
+
+# # find the value of the first derivative of the cubic spline interpolant at the given distance
+def cubic_spline_deriv():
+    return value
